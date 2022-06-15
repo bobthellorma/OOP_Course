c61f95052ae5313682ab60455223e3489d4ae4fa
bobthellorma <98739156+bobthellorma@users.noreply.github.com>
2022-06-15 17:21:34 +0930

    additional updates

--- abstractClasses.cpp
+++ abstractClasses.cpp
@@ -28,10 +28,34 @@ class Square : public Shape{
 
 void printShape(Shape *s){cout << s->get_area() << endl;}
 
-int main()
+void firstTest()
 {
     Shape *s[2];
     s[0] = new Square();
     s[1] = new Circle();
     printShape(s[0]);
+}
+
+template <class T, class K>
+class Point{
+    private:
+    T _x = 0;
+    T _y = 0;
+
+    public:
+    Point(T x, K y):_x(x),_y(y){};
+    void setPoint(T x, K y){_x=x,_y=y;}
+    T get_x(){return _x;}
+    K get_y(){return _y;}
+};
+
+typedef Point<int, int> Point_i;
+typedef Point_i* Point_i_ptr;
+
+int main()
+{
+    Point_i** ps;
+    ps[0] = new Point_i(0,0);
+    ps[1] = new Point_i(1,1);
+    cout << ps[0]->get_x() << "," << ps[0]->get_y() << endl; // core dumped as expected
 }
